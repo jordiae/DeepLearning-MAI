285c28873088ecb0dc61c6b20ca0f9d555023ac0
Jordi Armengol Estapé <me@jordiae.com>
2020-03-07 13:15:12 +0100

    Added script to get data.

--- get-mit67.sh
+++ get-mit67.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#wget http://groups.csail.mit.edu/vision/LabelMe/NewImages/indoorCVPR_09.tar -P data
-#mkdir data/mit67
-#tar -xvf data/indoorCVPR_09.tar -C data/mit67
+wget http://groups.csail.mit.edu/vision/LabelMe/NewImages/indoorCVPR_09.tar -P data
+mkdir data/mit67
+tar -xvf data/indoorCVPR_09.tar -C data/mit67
 rm data/indoorCVPR_09.tar
