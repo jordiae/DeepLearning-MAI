2e9e42c505a66b85f2daa6821c2f74a9c2de87c6
Jordi Armengol Estapé <me@jordiae.com>
2020-03-13 00:12:24 +0100

    Added module list

--- src/cnn/models/pyramid_cnn.py
+++ src/cnn/models/pyramid_cnn.py
@@ -12,7 +12,7 @@ class PyramidCNN(nn.Module):
         self.dropout_ = nn.Dropout()
         self.batch_norm = args.batch_norm
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv_layers = []
+        self.conv_layers = nn.ModuleList([])
         self.n_classes = 67
         self.input_size = 256
         self.stride = 1
@@ -28,12 +28,12 @@ class PyramidCNN(nn.Module):
             for j in range(0, args.conv_layers):
                 conv = nn.Conv2d(channels_in, channels_in*2, self.kernel_size, stride=self.stride, padding=self.padding)
                 if self.batch_norm:
-                    self.conv_layers.append((conv, nn.BatchNorm2d(channels_out)))
+                    self.conv_layers.append(nn.ModuleList([conv, nn.BatchNorm2d(channels_out)]))
                 else:
                     self.conv_layers.append(conv)
             channels_in = channels_out
 
-        self.fc_layers = []
+        self.fc_layers = nn.ModuleList([])
         # square image, so same dimensions
         dims_in = (self.input_size - self.kernel_size + 2*self.padding)*self.stride + 1
         self.dims_in_fc = dims_in
@@ -41,7 +41,7 @@ class PyramidCNN(nn.Module):
             dims_out = dims_in  # so far we keep it constant, but we could experiment with it
             fc = nn.Linear(dims_in, dims_out)
             if self.batch_norm:
-                self.fc_layers.append((fc, nn.BatchNorm1d(dims_out)))
+                self.fc_layers.append(nn.ModuleList([fc, nn.BatchNorm1d(dims_out)]))
             else:
                 self.fc_layers.append(fc)
             dims_in = dims_out
