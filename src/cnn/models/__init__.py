ce0c8625cb241a6451ed788f46941ab27783ff7a
Jordi Armengol Estapé <me@jordiae.com>
2020-03-12 23:18:58 +0100

    Fixed typo in init

--- src/cnn/models/__init__.py
+++ src/cnn/models/__init__.py
@@ -2,6 +2,6 @@ from .base_cnn import *
 from .alex_net import *
 from .five_layer_cnn import *
 from .albert_cnn import *
-from .pyramid_cnn import import *
+from .pyramid_cnn import *
 
 __all__ = ['BaseCNN', 'AlexNet', 'FiveLayerCNN', 'AlbertCNN', 'PyramidCNN']
