7b3673b4dcbb3579e28565614b2a5a845e4624e5
Jordi Armengol Estapé <me@jordiae.com>
2020-03-12 17:42:24 +0100

    Unindent script

--- src/cnn/create_experiment.py
+++ src/cnn/create_experiment.py
@@ -5,20 +5,20 @@ import sys
 
 def create_slurm_script(name, queue, time):
     script = f''' #!/bin/bash
-    #SBATCH --job-name="{name}"
-    #SBATCH --qos={queue}
-    #SBATCH --workdir=.
-    #SBATCH --output={name}_%j.out
-    #SBATCH --error={name}_%j.err
-    #SBATCH --cpus-per-task=40
-    #SBATCH --gres gpu:1
-    #SBATCH --time={time}
+#SBATCH --job-name="{name}"
+#SBATCH --qos={queue}
+#SBATCH --workdir=.
+#SBATCH --output={name}_%j.out
+#SBATCH --error={name}_%j.err
+#SBATCH --cpus-per-task=40
+#SBATCH --gres gpu:1
+#SBATCH --time={time}
 
-    module load gcc/8.3.0 cuda/10.1 cudnn/7.6.4 nccl/2.4.8 tensorrt/6.0.1 openmpi/4.0.1 atlas/3.10.3 scalapack/2.0.2
-    module load fftw/3.3.8 szip/2.1.1 ffmpeg/4.2.1 opencv/4.1.1
-    module load python/3.7.4_ML
-    
-    bash train.sh
+module load gcc/8.3.0 cuda/10.1 cudnn/7.6.4 nccl/2.4.8 tensorrt/6.0.1 openmpi/4.0.1 atlas/3.10.3 scalapack/2.0.2
+module load fftw/3.3.8 szip/2.1.1 ffmpeg/4.2.1 opencv/4.1.1
+module load python/3.7.4_ML
+
+bash train.sh
     '''
     return script
 
@@ -26,7 +26,7 @@ def create_slurm_script(name, queue, time):
 def create_train_script(parameters):
     relative_train_path = '/'.join(['..', '..', 'src', 'cnn', 'train.py'])
     script = f'''#!/bin/bash
-    python {relative_train_path} {parameters}'''
+python {relative_train_path} {parameters}'''
     return script
 
 
