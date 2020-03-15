2fda65aa03a5c0db589f4bec66bc36269a6ea6c8
Jordi Armengol Estapé <me@jordiae.com>
2020-03-15 13:02:13 +0100

    Added script to move to BSC clusters

---
+++ move_to_cluster_bsc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DATA_CLUSTER=dt01.bsc.es
+LOGIN_CLUSTER=plogin1.bsc.es
+
+read -p 'Username: ' USER
+
+# Deploy code and data
+RSYNC="rsync -a -e \"ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no\""
+DEPLOY_ADDRESS=${USER}@${DATA_CLUSTER}
+DEPLOY_DIR=/home/nct01/$USER/
+
+echo "$RSYNC data experiments src ${DEPLOY_ADDRESS}:${DEPLOY_DIR}"
+bash -c "$RSYNC data experiments src ${DEPLOY_ADDRESS}:${DEPLOY_DIR}"
+
+if [ $? -ne 0 ]
+then
+  echo "rsync error to ${DEPLOY_ADDRESS}"
+  exit 1
+fi
