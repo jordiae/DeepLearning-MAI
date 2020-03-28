849c8e0401cd793ff3791a209a26dee33e461e29
Jordi Armengol Estapé <me@jordiae.com>
2020-03-28 21:12:54 +0100

    Working on sorting the dataset by sequence length

--- src/rnn/dataset.py
+++ src/rnn/dataset.py
@@ -1,11 +1,13 @@
 from torch.utils.data.dataset import Dataset
 import os
 from typing import Tuple, List, Dict
+from tqdm import tqdm
+import numpy as np
 
 
 class MathDataset(Dataset):
     def __init__(self, path: str, subset: str, token2idx: Dict[str, int] = None, idx2token: List[str] = None,
-                 lower: bool = True, props: Tuple[int] = (98, 1, 1)):
+                 lower: bool = True, props: Tuple[int] = (98, 1, 1), debug: bool = False):
         """
 
         :param path: path to the 'train-easy' dataset of Deepmind's Mathematics dataset (v1.0)
@@ -16,24 +18,28 @@ class MathDataset(Dataset):
         :param lower: whether to convert characters to lowercase. By default, True, because characters don't seem to add
         information in this dataset
         :param props: proportions of the train-valid-test split
+        :param debug: load a tinty subset for debugging purpsoes
         """
         self.path = path
         self.subset = subset
         subsets = ['train', 'valid', 'test']
         self.props = props
         assert self.subset in subsets
-        assert token2idx is None if self.subset == 'train' else len(token2idx) > 1
-        assert idx2token is None if self.subset == 'train' else len(idx2token) > 1
+        assert token2idx is None if self.subset == 'train' else len(token2idx) > 0
+        assert idx2token is None if self.subset == 'train' else len(idx2token) > 0
         assert (token2idx is None and idx2token is None) or len(token2idx) == idx2token
         assert sum(self.props) == 100
         self.unk_token_idx = 0
         self.unk_token = '<UNK>'
-        self.token2idx = token2idx if token2idx is not None else {self.token2idx}
+        self.token2idx = token2idx if token2idx is not None else {self.unk_token: 0}
         self.idx2token = idx2token if token2idx is not None else [self.unk_token]
         self.proportions = dict(zip(subsets, self.props))
         self.X = []
         self.Y = []
         self.lower = lower
+        self.debug = debug
+        self.lines_debug = 100000
+        self.sorted = False
 
         def check_idx(i: int, sub: str) -> bool:
             """
@@ -52,10 +58,12 @@ class MathDataset(Dataset):
                 return True
             return False
 
-        for problem_type in os.listdir(path):
-            with open(os.path.join(path, problem_type)), 'r' as f:
+        self.lengths = []
+        for problem_type in tqdm(os.listdir(path), disable=self.debug):
+            with open(os.path.join(path, problem_type), 'r') as f:
                 idx_x = 0
-                for idx, line in enumerate(f.readlines()):
+                for idx, line in tqdm(enumerate(f.readlines()), disable=not self.debug,
+                                      total=self.lines_debug*(self.proportions[self.subset])//100):
                     if idx % 2 == 0:
                         if not check_idx(idx, self.subset):
                             idx_x += 1
@@ -68,10 +76,10 @@ class MathDataset(Dataset):
                                     self.__add_token(c)
                                 x.append(self.token2idx[c])
                         self.X.append(x)
+                        self.lengths.append(len(x))
                         idx_x += 1
                     else:
                         if not check_idx(idx, self.subset):
-                            idx_x += 1
                             continue
                         y = []
                         for c in line:
@@ -81,6 +89,11 @@ class MathDataset(Dataset):
                                     self.__add_token(c)
                                 y.append(self.token2idx[c])
                         self.Y.append(y)
+                        self.lengths[-1] += len(y)
+                    if self.debug and idx == self.lines_debug:
+                        break
+            if self.debug:
+                break
         self.data_len = len(self.X)
 
     def __getitem__(self, index: int) -> Tuple[List[int], List[int]]:
@@ -108,3 +121,23 @@ class MathDataset(Dataset):
             return
         self.idx2token.append(c)
         self.token2idx[c] = len(self.idx2token) - 1
+
+    def sort_by_lengths(self):
+        """
+        Sorts the dataset in increasing order of total length (X + Y), for efficiency purposes when generating batches
+        :return:
+        """
+        assert len(self.X) > 0
+        assert not self.sorted
+        sorted_idx = np.argsort(self.lengths)
+        self.X, self.Y = [(self.X[i], self.Y[i]) for i in sorted_idx]
+        self.sorted = True
+
+
+if __name__ == '__main__':
+    dataset = MathDataset(path=os.path.join('..', '..', 'data', 'mathematics', 'mathematics_dataset-v1.0',
+                                            'train-easy'), subset='train', debug=True)
+    print()
+    dataset.sort_by_lengths()
+    print()
+
