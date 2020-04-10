29ffdfec4c1155ed85251eef858c7fc00280a1a6
Albert Rial <albert.rial@est.fib.upc.edu>
2020-04-10 17:15:36 +0200

    Fixed typo

--- src/rnn/utils.py
+++ src/rnn/utils.py
@@ -12,10 +12,10 @@ def load_arch(vocab_size: Union[int, None], args: argparse.Namespace, log: loggi
             raise Exception('Vocabulary size already set')
         args.vocab_size = vocab_size
     if args.arch == 'elman':
-        model = VanillaRNN(vocab_size=args.vocab_size, embedding_size=args.embedding_size, hidden_dim=args.hidden_dize,
+        model = VanillaRNN(vocab_size=args.vocab_size, embedding_size=args.embedding_size, hidden_dim=args.hidden_size,
                            n_layers=args.n_layers, mode='elman')
     elif args.arch == 'jordan':
-        model = VanillaRNN(vocab_size=args.vocab_size, embedding_size=args.embedding_size, hidden_dim=args.hidden_dize,
+        model = VanillaRNN(vocab_size=args.vocab_size, embedding_size=args.embedding_size, hidden_dim=args.hidden_size,
                            n_layers=args.n_layers, mode='jordan')
     elif args.arch == 'AlbertRNN':
         model = AlbertRNN(vocab_size=args.vocab_size, embed_size=64, num_output=1, rnn_model='LSTM', use_last=True,
