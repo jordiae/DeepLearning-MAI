4d7a2911e5994a2f71a78400a0c47742640acd80
Jordi Armengol Estapé <me@jordiae.com>
2020-04-10 18:02:15 +0200

    Added LSTM to init

--- src/rnn/models/__init__.py
+++ src/rnn/models/__init__.py
@@ -1,5 +1,6 @@
 from .base_rnn import BaseRNN
 from .albert_rnn import AlbertRNN
 from .rnn import VanillaRNN
+from .lstm import LSTM
 
-__all__ = ['VanillaRNN', 'AlbertRNN']
+__all__ = ['VanillaRNN', 'LSTM', 'AlbertRNN']
